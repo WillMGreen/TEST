647572d8efa4bad1d3e24e2218e2d7f11e4321b4
William Green (s5066515) <s5066515@mac38574.mac.bournemouth.ac.uk>
2019-04-02 11:36:44 +0100

    Removed comments and added 'yo'

--- TEST/ViewController.swift
+++ TEST/ViewController.swift
@@ -1,18 +1,10 @@
-//
-//  ViewController.swift
-//  TEST
-//
-//  Created by William Green (s5066515) on 02/04/2019.
-//  Copyright © 2019 William Green (s5066515). All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // YO
     }
 
 
